cf6a6301bf81819f011110035481cee558779557
Frank Gaede <frank.gaede@desy.de>
2005-04-25 09:20:22 +0000

    inproved documentation

--- aida_env.sh
+++ aida_env.sh
@@ -6,7 +6,11 @@
 #####################################################
 
 
+
+# Note: if you don't want to use AIDA dont set MARLIN_USE_AIDA at all
+# if it is already set in your environment use 'unset MARLIN_USE_AIDA' !!
 export MARLIN_USE_AIDA=1
+
 
 # modify the following pathes as needed
 export JDK_HOME=/opt/products/java/1.4.2
