479ebcb8c456142392ad36bc1f3e1571a70738ec
Frank Gaede <frank.gaede@desy.de>
2005-06-09 10:40:54 +0000

    made aida setup part of env.sh - aida_env.sh no longer used

--- env.sh
+++ env.sh
@@ -38,5 +38,17 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CondDBMySQL/lib
 #--- fixes a bug in aida_env.sh with zsh (provided by J.Samson) :
 setopt shwordsplit > /dev/null 2>&1
 
-. aida_env.sh
+export MARLIN_USE_AIDA=1
+
+# modify the following pathes as needed
+export JDK_HOME=/opt/products/java/1.4.2
+
+export JAIDA_HOME=/opt/products/JAIDA/3.2.3
+
+export AIDAJNI_HOME=/opt/products/AIDAJNI/3.2.3
+
+. $JAIDA_HOME/bin/aida-setup.sh
+. $AIDAJNI_HOME/bin/Linux-g++/aidajni-setup.sh
+
+
 #------------------------------------------
