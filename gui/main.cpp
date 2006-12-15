196eb1924638dcc694ae1b63d258aa582c3c47f6
Jan Engels <jan.engels@desy.de>
2006-12-15 11:43:48 +0000

    changed the default black selection color to a nicer dark blue

--- gui/main.cpp
+++ gui/main.cpp
@@ -7,7 +7,12 @@ int main(int argc, char *argv[])
     std::cout << "\nPlease be patient, loading GUI...\n";
 	
     QApplication app(argc, argv);
-    
+
+    //set the selection color to blue instead of black ;)
+    QPalette pal = QApplication::palette();
+    pal.setColor(QPalette::Highlight, QColor(0,90,126) );
+    QApplication::setPalette(pal);
+
     MainWindow window;
 
     if( argc == 2 ){ 
