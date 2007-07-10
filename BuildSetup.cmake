6eb669a2f4218cd456dcc924441b393a94757544
Jan Engels <jan.engels@desy.de>
2007-07-10 09:51:12 +0000

    updated

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -1,7 +1,7 @@
 #############################################################################
 # cmake build setup for Marlin
 #
-# For building lcio with cmake type:
+# For building Marlin with cmake type:
 # (1) $ mkdir build
 # (2) $ cd build
 # (3) $ cmake -C ../BuildSetup.cmake ..
@@ -67,7 +67,6 @@ SET( RAIDA_HOME "${ILC_HOME}/RAIDA/HEAD"
 # Project options
 #############################################################################
 
-#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
 #SET( INSTALL_DOC OFF CACHE BOOL "Set to OFF to skip build/install Documentation" FORCE )
 #SET( MARLIN_USE_DLL OFF CACHE BOOL "Set to OFF to build Marlin without DLL support" FORCE )
 
@@ -78,6 +77,8 @@ SET( RAIDA_HOME "${ILC_HOME}/RAIDA/HEAD"
 #############################################################################
 # Advanced options
 #############################################################################
+
+#SET( BUILD_SHARED_LIBS OFF CACHE BOOL "Set to OFF to build static libraries" FORCE )
 
 # installation path for Marlin
 #SET( CMAKE_INSTALL_PREFIX "/foo/bar" CACHE STRING "Where to install Marlin" FORCE )
