a37c1b5da983684ab6c8318a3f01f03994f123da
Frank Gaede <frank.gaede@desy.de>
2007-11-07 15:18:42 +0000

    updated version numbers to ilcsoft release v01-03

--- examples/mymarlin/BuildSetup.cmake
+++ examples/mymarlin/BuildSetup.cmake
@@ -16,23 +16,23 @@
 #############################################################################
 
 # ILC_HOME
-SET( ILC_HOME "/afs/desy.de/group/it/ilcsoft/v01-01_beta"
+SET( ILC_HOME "/afs/desy.de/group/it/ilcsoft/v01-03"
     CACHE PATH "Path to ILC Software" FORCE )
 
 # Path to Marlin
-SET( Marlin_HOME "${ILC_HOME}/Marlin/HEAD"
+SET( Marlin_HOME "${ILC_HOME}/Marlin/v00-09-10"
     CACHE PATH "Path to Marlin" FORCE )
 
 # Path to LCIO
-SET( LCIO_HOME "${ILC_HOME}/lcio/HEAD"
+SET( LCIO_HOME "${ILC_HOME}/lcio/v01-08-05"
     CACHE PATH "Path to LCIO" FORCE )
 
 ## Path to GEAR
-#SET( GEAR_HOME "${ILC_HOME}/gear/HEAD"
+#SET( GEAR_HOME "${ILC_HOME}/gear/v00-07"
 #    CACHE PATH "Path to GEAR" FORCE )
 
 ## Path to LCCD
-#SET( LCCD_HOME "${ILC_HOME}/lccd/HEAD"
+#SET( LCCD_HOME "${ILC_HOME}/lccd/v00-03-06"
 #    CACHE PATH "Path to LCCD" FORCE )
 
 ## Path to CLHEP
@@ -40,11 +40,11 @@ SET( LCIO_HOME "${ILC_HOME}/lcio/HEAD"
 #    CACHE PATH "Path to CLHEP" FORCE )
 
 ## Path to RAIDA
-#SET( RAIDA_HOME "${ILC_HOME}/RAIDA/HEAD"
+#SET( RAIDA_HOME "${ILC_HOME}/RAIDA/v01-03"
 #    CACHE PATH "Path to RAIDA" FORCE )
 
 # CMake Modules Path
-SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-00"
+SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-04"
     CACHE PATH "Path to CMake Modules" FORCE )
 
 
