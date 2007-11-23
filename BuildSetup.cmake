a92c9cd8776123a5e85728b3c79a65d27ce2d8ba
Jan Engels <jan.engels@desy.de>
2007-11-23 15:24:52 +0000

    updated for ilcsoft release v01-03

--- BuildSetup.cmake
+++ BuildSetup.cmake
@@ -24,7 +24,7 @@ SET( LCIO_HOME "${ILC_HOME}/lcio/v01-09"
     CACHE PATH "Path to LCIO" FORCE )
 
 # CMake Modules Path
-SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-04"
+SET( CMAKE_MODULE_PATH "${ILC_HOME}/CMakeModules/v01-04-01"
     CACHE PATH "Path to CMake Modules" FORCE )
 
 #############################################################################
