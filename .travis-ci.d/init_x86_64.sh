2cec1f03c2b9617d92cf3079389055c353f7b040
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-12-05 18:35:50 +0100

    Add travis configuration

---
+++ .travis-ci.d/init_x86_64.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#--------------------------------------------------------------------------------
+#     ILCSOFT
+#--------------------------------------------------------------------------------
+
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_gcc
+source $ILCSOFT/init_ilcsoft.sh
