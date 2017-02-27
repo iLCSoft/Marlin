31266bbb68a800aef6fd502c35bf9dffed053bb8
Andre Sailer <andre.philippe.sailer@cern.ch>
2017-02-27 15:45:34 +0100

    Add diagnostics for travis

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -C $ILCSOFT/ILCSoft.cmake ..
+cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" ..
 ninja
 ctest --output-on-failure
 
