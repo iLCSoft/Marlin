e074a3955197f27ae136469d1b68032cf84db6bd
Ete Remi <remi.ete@gmail.com>
2017-11-30 15:47:52 +0100

    Added -Werror flag in Travis and -k0 for ninja

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -6,7 +6,7 @@ source $ILCSOFT/init_ilcsoft.sh
 cd /Package
 mkdir build
 cd build
-cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
-ninja && \
+cmake -GNinja -C $ILCSOFT/ILCSoft.cmake -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" .. && \
+ninja -k0 && \
 ninja install && \
 ctest --output-on-failure
