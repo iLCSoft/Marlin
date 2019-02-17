fed08271c9c396d90b991b5328953a5685fe46c1
Remi Ete <remi.ete@gmail.com>
2019-02-17 21:35:42 +0100

    Added PluginManager class

---
+++ source/include/marlin/concurrency/PluginManager.h
@@ -0,0 +1,103 @@
+#ifndef MARLIN_CONCURRENCY_PLUGINMANAGER_h
+#define MARLIN_CONCURRENCY_PLUGINMANAGER_h 1
+
+// -- std headers
+#include <map>
+#include <string>
+
+// -- marlin headers
+#include "marlin/Exceptions.h"
+#include "marlin/concurrency/ProcessorFactory.h"
+
+// registration macros
+#define MARLIN_REGISTER_PROCESSOR_NAME( Class, Name ) \
+  { PluginManager::instance().registerFactory<Class>( Name ) ; }
+
+#define MARLIN_REGISTER_PROCESSOR( Class ) \
+  MARLIN_REGISTER_PROCESSOR_NAME( Class, #Class )
+
+namespace marlin {
+
+  namespace concurrency {
+
+    /**
+     *  @brief  PluginManager singleton class
+     *  Responsible for loading shared libraries and collecting
+     *  processor factory instances. Processor instances can be
+     *  created from factories using the PluginManager::create()
+     *  method on query.
+     */
+    class PluginManager {
+    public:
+      // traits
+      typedef std::shared_ptr<ProcessorFactory>           FactoryType ;
+      typedef std::map<const std::string, FactoryType>    FactoryMap ;
+      typedef std::vector<void*>                          LibraryList ;
+
+    private:
+      PluginManager(const PluginManager &) = delete ;
+      PluginManager& operator=(const PluginManager &) = delete ;
+      PluginManager() = default ;
+      ~PluginManager() = default ;
+
+      /**
+       *  @brief  Register a new processor factory
+       *  The template parameter is the processor type
+       *
+       *  @param  name the factory
+       */
+      template <typename T>
+      void registerFactory( const std::string &name ) ;
+
+    public:
+      /**
+       *  @brief  Get the plugin manager instance
+       */
+      static PluginManager &instance() ;
+
+      /**
+       *  @brief  Load shared libraries to populate the list of plugins
+       *
+       *  @param  envvar the environment variable to load the libraries from
+       */
+      bool loadLibraries( const std::string &envvar = "MARLIN_DLL" ) ;
+
+      /**
+       *  @brief  Get the list of registered processor factories
+       *  The list of returned names corresponds to the processor types
+       */
+      std::vector<std::string> factoryNames() const ;
+
+      /**
+       *  @brief  Create a processor instance
+       *
+       *  @param  type the processor type
+       *  @param  name the processor name
+       */
+      std::shared_ptr<Processor> create( const std::string &type, const std::string &name );
+
+    private:
+      /// The map of processor factories
+      FactoryMap              _factories {} ;
+      /// The list of loaded libraries
+      LibraryList             _libraries {} ;
+    };
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+
+    template <typename T>
+    inline void PluginManager::registerFactory( const std::string &name ) {
+      auto iter = _factories.find( name ) ;
+      if ( _factories.end() != iter ) {
+        throw Exception( "PluginManager::registerFactory: factory '" + name + "' already registered!" );
+      }
+      FactoryType factory = std::make_shared<ProcessorFactoryT<T>>() ;
+      _factories.insert( FactoryMap::value_type( name, factory ) );
+    }
+
+  } // end namespace concurrency
+
+} // end namespace marlin
+
+#endif
---
+++ source/src/concurrency/PluginManager.cc
@@ -0,0 +1,72 @@
+#include "marlin/concurrency/PluginManager.h"
+#include "marlin/concurrency/Internal.h"
+
+// -- std headers
+#include <dlfcn.h>
+#include <iostream>
+#include <cstdlib>
+#include <set>
+
+namespace marlin {
+
+  namespace concurrency {
+
+    std::vector<std::string> PluginManager::factoryNames() const {
+      std::vector<std::string> names ;
+      for ( auto iter : _factories ) {
+        names.push_back( iter.first ) ;
+      }
+      return names ;
+    }
+
+    //--------------------------------------------------------------------------
+
+    PluginManager &PluginManager::instance() {
+      static PluginManager mgr;
+      return mgr;
+    }
+
+    //--------------------------------------------------------------------------
+
+    bool PluginManager::loadLibraries( const std::string &envvar )  {
+      char *marlinDll = getenv( envvar.c_str() );
+      if ( nullptr == marlinDll ) {
+        // TODO log a debug message here!
+        return true ;
+      }
+      std::vector<std::string> libraries = StringUtil::split<std::string>( marlinDll , ":" ) ;
+      std::set<std::string> checkDuplicateLibs;
+      for ( auto library : libraries ) {
+        size_t idx = library.find_last_of("/") ;
+        // the library basename, i.e. /path/to/libBlah.so --> libBlah.so
+        std::string libBaseName( library.substr( idx + 1 ) );
+        auto inserted = checkDuplicateLibs.insert( libBaseName ).second ;
+        if ( not inserted ) {
+          std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
+              << "    ->    Trying to load DUPLICATE library -->" << std::endl << std::endl ;
+          return false ;
+        }
+        void* libPointer = dlopen( library.c_str() , RTLD_LAZY | RTLD_GLOBAL) ;
+        if( nullptr == libPointer ) {
+          std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
+                      << "    ->    "   << dlerror() << " -->" << std::endl << std::endl ;
+          return false ;
+        }
+        _libraries.push_back( libPointer ) ;
+      }
+      return true ;
+    }
+
+    //--------------------------------------------------------------------------
+
+    std::shared_ptr<Processor> PluginManager::create( const std::string &type, const std::string &name ) {
+      auto iter = _factories.find( type ) ;
+      if ( _factories.end() == iter ) {
+        return nullptr;
+      }
+      return iter->second->create( name );
+    }
+
+  } // namespace concurrency
+
+} // namespace marlin
