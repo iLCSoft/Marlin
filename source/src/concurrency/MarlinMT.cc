057bbdaf297f77d3fe1bfe5612aacaa5922d6c97
Remi Ete <remi.ete@gmail.com>
2019-02-27 16:07:13 +0900

    Create instance of MarlinMT application, initialize it and run it

--- source/src/concurrency/MarlinMT.cc
+++ source/src/concurrency/MarlinMT.cc
@@ -1,36 +1,29 @@
-#include "lcio.h"
+// -- std headers
+#include <memory>
 
+// -- marlin headers
 #include "marlin/concurrency/PluginManager.h"
-
-int printUsage() ;
+#include "marlin/concurrency/Application.h"
 
 using PluginManager = marlin::concurrency::PluginManager ;
+using Application = marlin::concurrency::Application ;
 
 int main(int argc, char** argv ) {
 
   // ---- catch all uncaught exceptions in the end ...
   try {
-
+    // load plugins first
     auto &mgr = PluginManager::instance() ;
     if ( not mgr.loadLibraries() ) {
       throw Exception( "Couldn't load shared libraries from MARLIN_DLL !" );
     }
-
-    if( argc > 1 ) {
-      if( std::string(argv[1]) == "-p" ){
-        mgr.dump() ;
-        return(0) ;
-      }
-      else if( std::string(argv[1]) == "-h"  || std::string(argv[1]) == "-?" ){
-        return printUsage() ;
-      }
-    }
-    else {
-      return printUsage() ;
-    }
+    // create, init and run the MarlinMT application
+    auto app = std::make_shared<Application>() ;
+    app->init( argc, argv ) ;
+    app->run() ;
     return 0 ;
-
-  } catch( std::exception& e) {
+  }
+  catch( std::exception& e) {
 
     std::cerr << " ***********************************************\n"
 	      << " A runtime error occured - (uncaught exception):\n"
@@ -40,18 +33,4 @@ int main(int argc, char** argv ) {
 	      << std:: endl ;
   }
   return 1 ;
-}
-
-int printUsage() {
-
-  std::cout << " Usage: MarlinMT [OPTION]" << std::endl
-	    << "   runs a parallel Marlin application " << std::endl
-	    << std::endl
-      << "   Marlin [-h/-?]             \t print this help information" << std::endl
-	    << "   Marlin -p                  \t dump the registered plugins and exit" << std::endl
-      << "   Marlin -j N                \t set the number of cores to use for parallel processing" << std::endl
-	    << std::endl ;
-
-  return(0) ;
-
 }
