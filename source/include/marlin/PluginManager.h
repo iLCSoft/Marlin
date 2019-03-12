402f0dd350d4207d3b3e4579169121b894f25845
Ete Remi <remi.ete@desy.de>
2019-03-12 14:47:46 +0100

    Moved PluginManager to standard Marlin include directory

--- source/include/marlin/PluginManager.h
+++ source/include/marlin/PluginManager.h
@@ -1,5 +1,5 @@
-#ifndef MARLIN_CONCURRENCY_PLUGINMANAGER_h
-#define MARLIN_CONCURRENCY_PLUGINMANAGER_h 1
+#ifndef MARLIN_PLUGINMANAGER_h
+#define MARLIN_PLUGINMANAGER_h 1
 
 // -- std headers
 #include <map>
@@ -7,101 +7,78 @@
 
 // -- marlin headers
 #include "marlin/Exceptions.h"
-#include "marlin/concurrency/ProcessorFactory.h"
-
-// registration macros
-#define MARLIN_REGISTER_PROCESSOR_NAME( Class, Name ) \
-  { PluginManager::instance().registerProcessorFactory<Class>( Name ) ; }
-
-#define MARLIN_REGISTER_PROCESSOR( Class ) \
-  MARLIN_REGISTER_PROCESSOR_NAME( Class, #Class )
 
 namespace marlin {
 
-  namespace concurrency {
+  /**
+   *  @brief  PluginManager singleton class
+   *  Responsible for loading shared libraries and collecting
+   *  processor factory instances. Processor instances can be
+   *  created from factories using the PluginManager::create()
+   *  method on query.
+   */
+  class PluginManager {
+    // TODO find a better mechanism for plugins
+    friend class Processor ; // for registration
+    
+  public:
+    // typedefs
+    typedef Processor*                                  FactoryType ;
+    typedef std::map<const std::string, FactoryType>    FactoryMap ;
+    typedef std::vector<void*>                          LibraryList ;
+
+  private:
+    PluginManager(const PluginManager &) = delete ;
+    PluginManager& operator=(const PluginManager &) = delete ;
+    PluginManager() = default ;
+    ~PluginManager() = default ;
+    
+    /**
+     *  @brief  Register a new processor factory
+     *
+     *  @param  proc the prototype processor used as factory
+     */
+    void registerProcessorFactory( Processor *proc ) ;
+
+  public:
+    /**
+     *  @brief  Get the plugin manager instance
+     */
+    static PluginManager &instance() ;
 
     /**
-     *  @brief  PluginManager singleton class
-     *  Responsible for loading shared libraries and collecting
-     *  processor factory instances. Processor instances can be
-     *  created from factories using the PluginManager::create()
-     *  method on query.
+     *  @brief  Load shared libraries to populate the list of plugins
+     *
+     *  @param  envvar the environment variable to load the libraries from
      */
-    class PluginManager {
-    public:
-      // traits
-      typedef std::shared_ptr<ProcessorFactory>           FactoryType ;
-      typedef std::map<const std::string, FactoryType>    FactoryMap ;
-      typedef std::vector<void*>                          LibraryList ;
+    bool loadLibraries( const std::string &envvar = "MARLIN_DLL" ) ;
 
-    private:
-      PluginManager(const PluginManager &) = delete ;
-      PluginManager& operator=(const PluginManager &) = delete ;
-      PluginManager() = default ;
-      ~PluginManager() = default ;
+    /**
+     *  @brief  Get the list of registered processor factories
+     *  The list of returned names corresponds to the processor types
+     */
+    std::vector<std::string> processorTypes() const ;
 
-      /**
-       *  @brief  Register a new processor factory
-       *  The template parameter is the processor type
-       *
-       *  @param  name the factory
-       */
-      template <typename T>
-      void registerProcessorFactory( const std::string &name ) ;
+    /**
+     *  @brief  Create a new processor instance.
+     *  Ownership transfered to the called
+     *
+     *  @param  type the processor type
+     *  @param  name the processor name
+     */
+    Processor* createProcessor( const std::string &type, const std::string &name ) ;
 
-    public:
-      /**
-       *  @brief  Get the plugin manager instance
-       */
-      static PluginManager &instance() ;
+    /**
+     *  @brief  Dump plugin manager content in console
+     */
+    void dump() const ;
 
-      /**
-       *  @brief  Load shared libraries to populate the list of plugins
-       *
-       *  @param  envvar the environment variable to load the libraries from
-       */
-      bool loadLibraries( const std::string &envvar = "MARLIN_DLL" ) ;
-
-      /**
-       *  @brief  Get the list of registered processor factories
-       *  The list of returned names corresponds to the processor types
-       */
-      std::vector<std::string> processorFactoryNames() const ;
-
-      /**
-       *  @brief  Create a processor instance
-       *
-       *  @param  type the processor type
-       *  @param  name the processor name
-       */
-      std::shared_ptr<Processor> createProcessor( const std::string &type, const std::string &name );
-
-      /**
-       *  @brief  Dump plugin manager content in console
-       */
-      void dump() const ;
-
-    private:
-      /// The map of processor factories
-      FactoryMap              _factories {} ;
-      /// The list of loaded libraries
-      LibraryList             _libraries {} ;
-    };
-
-    //--------------------------------------------------------------------------
-    //--------------------------------------------------------------------------
-
-    template <typename T>
-    inline void PluginManager::registerProcessorFactory( const std::string &name ) {
-      auto iter = _factories.find( name ) ;
-      if ( _factories.end() != iter ) {
-        throw Exception( "PluginManager::registerFactory: factory '" + name + "' already registered!" );
-      }
-      FactoryType factory = std::make_shared<ProcessorFactoryT<T>>() ;
-      _factories.insert( FactoryMap::value_type( name, factory ) );
-    }
-
-  } // end namespace concurrency
+  private:
+    /// The map of processor factories
+    FactoryMap              _factories {} ;
+    /// The list of loaded libraries
+    LibraryList             _libraries {} ;
+  };
 
 } // end namespace marlin
 
--- source/src/PluginManager.cc
+++ source/src/PluginManager.cc
@@ -1,89 +1,104 @@
-#include "marlin/concurrency/PluginManager.h"
-#include "marlin/concurrency/Internal.h"
+#include "marlin/PluginManager.h"
 
 // -- std headers
 #include <dlfcn.h>
 #include <iostream>
 #include <cstdlib>
+#include <algorithm>
 #include <set>
 
+// -- lcio headers
+#include <UTIL/BitField64.h>  // LCTokenizer
+
 namespace marlin {
-
-  namespace concurrency {
-
-    std::vector<std::string> PluginManager::processorFactoryNames() const {
-      std::vector<std::string> names ;
-      for ( auto iter : _factories ) {
-        names.push_back( iter.first ) ;
-      }
-      return names ;
+  
+  void PluginManager::registerProcessorFactory( Processor *proc ) {
+    auto iter = _factories.find( proc->type() ) ;
+    if ( _factories.end() != iter ) {
+      throw Exception( "PluginManager::registerFactory: factory '" + proc->type() + "' already registered!" );
     }
+    _factories.insert( FactoryMap::value_type( proc->name(), proc ) ) ;
+  }
+  
+  //--------------------------------------------------------------------------
 
-    //--------------------------------------------------------------------------
-
-    PluginManager &PluginManager::instance() {
-      static PluginManager mgr;
-      return mgr;
+  std::vector<std::string> PluginManager::processorTypes() const {
+    std::vector<std::string> names ;
+    for ( auto iter : _factories ) {
+      names.push_back( iter.first ) ;
     }
+    return names ;
+  }
 
-    //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
 
-    bool PluginManager::loadLibraries( const std::string &envvar )  {
-      char *marlinDll = getenv( envvar.c_str() );
-      if ( nullptr == marlinDll ) {
-        // TODO log a debug message here!
-        return true ;
-      }
-      std::vector<std::string> libraries = StringUtil::split<std::string>( marlinDll , ":" ) ;
-      std::set<std::string> checkDuplicateLibs;
-      for ( auto library : libraries ) {
-        size_t idx = library.find_last_of("/") ;
-        // the library basename, i.e. /path/to/libBlah.so --> libBlah.so
-        std::string libBaseName( library.substr( idx + 1 ) );
-        auto inserted = checkDuplicateLibs.insert( libBaseName ).second ;
-        if ( not inserted ) {
-          std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
-              << "    ->    Trying to load DUPLICATE library -->" << std::endl << std::endl ;
-          return false ;
-        }
-        void* libPointer = dlopen( library.c_str() , RTLD_LAZY | RTLD_GLOBAL) ;
-        if( nullptr == libPointer ) {
-          std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
-                      << "    ->    "   << dlerror() << " -->" << std::endl << std::endl ;
-          return false ;
-        }
-        _libraries.push_back( libPointer ) ;
-      }
+  PluginManager &PluginManager::instance() {
+    static PluginManager mgr;
+    return mgr;
+  }
+
+  //--------------------------------------------------------------------------
+
+  bool PluginManager::loadLibraries( const std::string &envvar )  {
+    char *marlinDll = getenv( envvar.c_str() );
+    if ( nullptr == marlinDll ) {
+      // TODO log a debug message here!
       return true ;
     }
-
-    //--------------------------------------------------------------------------
-
-    std::shared_ptr<Processor> PluginManager::createProcessor( const std::string &type, const std::string &name ) {
-      auto iter = _factories.find( type ) ;
-      if ( _factories.end() == iter ) {
-        return nullptr;
+    std::string marlinDllStr (marlinDll) ;    
+    std::vector<std::string> libraries ;
+    LCTokenizer tokenizer ( libraries , ':' ) ;
+    std::for_each( marlinDllStr.begin() , marlinDllStr.end() , tokenizer ) ;
+    
+    std::set<std::string> checkDuplicateLibs;
+    for ( auto library : libraries ) {
+      size_t idx = library.find_last_of("/") ;
+      // the library basename, i.e. /path/to/libBlah.so --> libBlah.so
+      std::string libBaseName( library.substr( idx + 1 ) );
+      auto inserted = checkDuplicateLibs.insert( libBaseName ).second ;
+      if ( not inserted ) {
+        std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
+            << "    ->    Trying to load DUPLICATE library -->" << std::endl << std::endl ;
+        return false ;
       }
-      return iter->second->create( name );
+      void* libPointer = dlopen( library.c_str() , RTLD_LAZY | RTLD_GLOBAL) ;
+      if( nullptr == libPointer ) {
+        std::cerr << std::endl << "<!-- ERROR loading shared library : " << library << std::endl
+                    << "    ->    "   << dlerror() << " -->" << std::endl << std::endl ;
+        return false ;
+      }
+      _libraries.push_back( libPointer ) ;
     }
+    return true ;
+  }
 
-    //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
 
-    void PluginManager::dump() const {
-      std::cout << "------------------------------------" << std::endl ;
-      std::cout << " ** MarlinMT plugin manager dump ** " << std::endl ;
-      if ( _factories.empty() ) {
-        std::cout << " No processor registered ..." << std::endl ;
-      }
-      else {
-        std::cout << " + Registered processors: " << std::endl ;
-        for ( auto iter : _factories ) {
-          std::cout << "   - " << iter.first << std::endl ;
-        }
-      }
-      std::cout << "----------------------------------" << std::endl ;
+  Processor* PluginManager::createProcessor( const std::string &type, const std::string &name ) {
+    auto iter = _factories.find( type ) ;
+    if ( _factories.end() == iter ) {
+      return nullptr;
     }
+    auto proc = iter->second->newProcessor() ;
+    proc->setName( name ) ;
+    return proc ;
+  }
 
-  } // namespace concurrency
+  //--------------------------------------------------------------------------
+
+  void PluginManager::dump() const {
+    std::cout << "------------------------------------" << std::endl ;
+    std::cout << " ** MarlinMT plugin manager dump ** " << std::endl ;
+    if ( _factories.empty() ) {
+      std::cout << " No processor registered ..." << std::endl ;
+    }
+    else {
+      std::cout << " + Registered processors: " << std::endl ;
+      for ( auto iter : _factories ) {
+        std::cout << "   - " << iter.first << std::endl ;
+      }
+    }
+    std::cout << "----------------------------------" << std::endl ;
+  }
 
 } // namespace marlin
