70d214d2e6675ba744019c23e5c6c58abe7ea7ec
Remi Ete <remi.ete@gmail.com>
2019-03-26 19:01:34 +0100

    Fixed compilation errors

--- source/src/GearGeometry.cc
+++ source/src/GearGeometry.cc
@@ -10,39 +10,39 @@ namespace marlin {
   GearGeometry::GearGeometry() :
     GeometryPlugin("Gear") {
     // compact file
-    registerParameter( "GearFile", 
+    registerParameter( "GearFile",
       "The GEAR geometry XML file",
       _gearFile,
-      "") ;
+      std::string("")) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void GearGeometry::loadGeometry() {
     gear::GearXML gearXML( _gearFile ) ;
     _gearMgr.reset( gearXML.createGearMgr() ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   const void *GearGeometry::handle() const {
     return _gearMgr.get() ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void GearGeometry::destroy() {
     _gearMgr = nullptr ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   std::type_index GearGeometry::typeIndex() const {
     return std::type_index( typeid( gear::GearMgr ) ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void GearGeometry::dumpGeometry() const {
     _logger->log<DEBUG5>() << *_gearMgr << std::endl ;
   }
