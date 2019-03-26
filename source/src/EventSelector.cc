32225e05077193653a2b314614b8ae2a88f98bb7
Remi Ete <remi.ete@gmail.com>
2019-03-26 19:01:21 +0100

    Changed logger calls. Uses processor logger instead

--- source/src/EventSelector.cc
+++ source/src/EventSelector.cc
@@ -2,9 +2,8 @@
 
 #include <iostream>
 
-// ----- include for verbosity dependend logging ---------
-#include "marlin/VerbosityLevels.h"
-
+// -- marlin headers
+#include <marlin/Logging.h>
 
 using namespace lcio ;
 using namespace marlin ;
@@ -14,52 +13,52 @@ EventSelector aEventSelector ;
 
 
 EventSelector::EventSelector() : Processor("EventSelector") {
-  
+
   // modify processor description
   _description = "EventSelector returns true if given event was specified in EventList" ;
-  
+
   IntVec evtsExample ;
   // evtsExample.push_back( 42 );
   // evtsExample.push_back( 1024 );
-  
-  
-  registerProcessorParameter( "EventList" , 
+
+
+  registerProcessorParameter( "EventList" ,
 			      "event list - pairs of Eventnumber RunNumber"  ,
 			      _evtList ,
 			      evtsExample ) ;
   _nEvt = -1;
-  _nRun = -1;  
+  _nRun = -1;
 }
 
 
-void EventSelector::init() { 
-  
+void EventSelector::init() {
+
   // usually a good idea to
   printParameters() ;
-  
+
   _nRun = 0 ;
   _nEvt = 0 ;
-  
+
 
   unsigned nEvts = _evtList.size() ;
 
   for( unsigned i=0 ; i < nEvts ; i+=2 ) {
-    
+
     _evtSet.insert( std::make_pair( _evtList[i] , _evtList[ i+1 ] ) ) ;
   }
 }
 
-void EventSelector::processRunHeader( LCRunHeader* ) { 
-  
+void EventSelector::processRunHeader( LCRunHeader* ) {
+
   _nRun++ ;
-} 
+}
 
 
 void EventSelector::modifyEvent( LCEvent *evt ) {
   processEvent( evt );
 }
 
-void EventSelector::processEvent( LCEvent * evt ) { 
+void EventSelector::processEvent( LCEvent * evt ) {
 
   // if no events specifiec - always return true
   if( _evtList.size() == 0 ) {
@@ -69,14 +68,14 @@ void EventSelector::processEvent( LCEvent * evt ) {
 
   SET::iterator it = _evtSet.find( std::make_pair( evt->getEventNumber() , evt->getRunNumber() ) ) ;
 
-  bool isInList = it != _evtSet.end() ; 
-    
+  bool isInList = it != _evtSet.end() ;
+
 
   //-- note: this will not be printed if compiled w/o MARLINDEBUG=1 !
 
-  streamlog_out(DEBUG) << "   processing event: " << evt->getEventNumber() 
-		       << "   in run:  " << evt->getRunNumber() 
-		       << " - in event list : " << isInList 
+  log<DEBUG>() << "   processing event: " << evt->getEventNumber()
+		       << "   in run:  " << evt->getRunNumber()
+		       << " - in event list : " << isInList
 		       << std::endl ;
 
   setReturnValue( isInList ) ;
@@ -87,16 +86,15 @@ void EventSelector::processEvent( LCEvent * evt ) {
 
 
 
-void EventSelector::check( LCEvent *  ) { 
+void EventSelector::check( LCEvent *  ) {
   // nothing to check here - could be used to fill checkplots in reconstruction processor
 }
 
 
-void EventSelector::end(){ 
-  
-//   std::cout << "EventSelector::end()  " << name() 
+void EventSelector::end(){
+
+//   std::cout << "EventSelector::end()  " << name()
 // 	    << " processed " << _nEvt << " events in " << _nRun << " runs "
 // 	    << std::endl ;
 
 }
-
