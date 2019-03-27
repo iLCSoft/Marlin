045a412b81b76ab56962a8744db6ce295b339a4d
Ete Remi <remi.ete@desy.de>
2019-03-27 20:12:46 +0100

    Changed verbosity level in dump detector from DEBUG5 to MESSAGE

--- source/src/DD4hepGeometry.cc
+++ source/src/DD4hepGeometry.cc
@@ -69,27 +69,27 @@ namespace marlin {
   void DD4hepGeometry::printDetectorData( DetElement det ) const {
     try {
       FixedPadSizeTPCData* d = det.extension<FixedPadSizeTPCData>() ;
-      _logger->log<DEBUG5>() << *d ;
+      _logger->log<MESSAGE>() << *d ;
     }
     catch(...){}
     try {
       ZPlanarData* d = det.extension<ZPlanarData>() ;
-      _logger->log<DEBUG5>() << *d ;
+      _logger->log<MESSAGE>() << *d ;
     }
     catch(...){}
     try {
       ZDiskPetalsData* d = det.extension<ZDiskPetalsData>() ;
-      _logger->log<DEBUG5>() << *d ;
+      _logger->log<MESSAGE>() << *d ;
     }
     catch(...){}
     try {
       ConicalSupportData* d = det.extension<ConicalSupportData>() ;
-      _logger->log<DEBUG5>() << *d ;
+      _logger->log<MESSAGE>() << *d ;
     }
     catch(...){}
     try {
       LayeredCalorimeterData* d = det.extension<LayeredCalorimeterData>() ;
-      _logger->log<DEBUG5>() << *d ;
+      _logger->log<MESSAGE>() << *d ;
     }
     catch(...){}
   }
@@ -99,20 +99,20 @@ namespace marlin {
   void DD4hepGeometry::printDetectorSets( const std::string &name, unsigned int includeFlag,  unsigned int excludeFlag ) const {
     Detector& description = Detector::getInstance();
     const std::vector<DetElement>& dets = DetectorSelector(description).detectors( includeFlag, excludeFlag ) ;
-    _logger->log<DEBUG5>() << " " << name  ;
+    _logger->log<MESSAGE>() << " " << name  ;
     for(int i=0,N=dets.size();i<N;++i) {
-      _logger->log<DEBUG5>() << dets[i].name() << ", " ;
+      _logger->log<MESSAGE>() << dets[i].name() << ", " ;
     }
-    _logger->log<DEBUG5>() << std::endl ;
+    _logger->log<MESSAGE>() << std::endl ;
   }
 
   //--------------------------------------------------------------------------
 
   void DD4hepGeometry::dumpGeometry() const {
     Detector& description = Detector::getInstance();
-    _logger->log<DEBUG5>() << "############################################################################### "  << std::endl  ;
+    _logger->log<MESSAGE>() << "############################################################################### "  << std::endl  ;
     Header h = description.header() ;
-    _logger->log<DEBUG5>()
+    _logger->log<MESSAGE>()
         << " detector model : " <<  h.name()  << std::endl
   	    << "    title : "  << h.title() << std::endl
   	    << "    author : " << h.author() << std::endl
@@ -128,23 +128,23 @@ namespace marlin {
     printDetectorSets( " other detecors : " , ( DetType::IGNORE ) , ( DetType::CALORIMETER | DetType::TRACKER ) ) ;
     // dump detector data
     if( _dumpDetectorData ) {
-      _logger->log<DEBUG5>() << "############################################################################### "  << std::endl  ;
+      _logger->log<MESSAGE>() << "############################################################################### "  << std::endl  ;
       dd4hep::Detector::HandleMap dets = description.detectors() ;
       for( dd4hep::Detector::HandleMap::const_iterator it = dets.begin() ; it != dets.end() ; ++it ) {
         DetElement det = it->second ;
-        _logger->log<DEBUG5>() << " ---------------------------- " << det.name() << " ----------------------------- " << std::endl ;
+        _logger->log<MESSAGE>() << " ---------------------------- " << det.name() << " ----------------------------- " << std::endl ;
         DetType type( det.typeFlag() ) ;
-        _logger->log<DEBUG5>() << " ------     " << type << std:: endl ;
+        _logger->log<MESSAGE>() << " ------     " << type << std:: endl ;
         printDetectorData( det ) ;
       }
-      _logger->log<DEBUG5>() << "############################################################################### "  << std::endl  ;
+      _logger->log<MESSAGE>() << "############################################################################### "  << std::endl  ;
     }
     dd4hep::Detector::HandleMap sensDet = description.sensitiveDetectors() ;
-    _logger->log<DEBUG5>() << "############################################################################### "  << std::endl ;
-    _logger->log<DEBUG5>() << "     sensitive detectors:     " << std::endl ;
+    _logger->log<MESSAGE>() << "############################################################################### "  << std::endl ;
+    _logger->log<MESSAGE>() << "     sensitive detectors:     " << std::endl ;
     for( dd4hep::Detector::HandleMap::const_iterator it = sensDet.begin() ; it != sensDet.end() ; ++it ){
       SensitiveDetector sDet = it->second ;
-      _logger->log<DEBUG5>() << "     " << it->first << " : type = " << sDet.type() << std::endl ;
+      _logger->log<MESSAGE>() << "     " << it->first << " : type = " << sDet.type() << std::endl ;
     }
     std::cout << "############################################################################### "  << std::endl  << std::endl  ;
   	DetElement world = description.world() ;
@@ -178,11 +178,11 @@ namespace marlin {
       }
       SurfaceHelper surfMan( de ) ;
       const SurfaceList& sL = surfMan.surfaceList() ;
-      _logger->log<DEBUG5>() << "DetElement: " ;
+      _logger->log<MESSAGE>() << "DetElement: " ;
       for(unsigned i=0 ; i < parentCount ; ++i ) {
-        _logger->log<DEBUG5>() << "\t" ;
+        _logger->log<MESSAGE>() << "\t" ;
       }
-      _logger->log<DEBUG5>()
+      _logger->log<MESSAGE>()
         << de.name()
         << "[ path: "<< de.placementPath ()  <<  "] (id: " << de.id() << ") - sens type : "
         << description.sensitiveDetector( de.name() ).type()
@@ -191,13 +191,13 @@ namespace marlin {
       if( _dumpDetectorSurfaces ) {
         for( SurfaceList::const_iterator sit = sL.begin() ; sit != sL.end() ; ++sit ) {
         	const ISurface* surf =  *sit ;
-        	_logger->log<DEBUG5>() << " ------------------------- "
+        	_logger->log<MESSAGE>() << " ------------------------- "
         		  << " surface: "  << *surf         << std::endl
         		  << " ------------------------- "  << std::endl ;
         }
       }
     }
-    _logger->log<DEBUG5>() << "############################################################################### "  << std::endl  << std::endl  ;
+    _logger->log<MESSAGE>() << "############################################################################### "  << std::endl  << std::endl  ;
   }
   
   MARLIN_DECLARE_GEOPLUGIN( DD4hepGeometry )
