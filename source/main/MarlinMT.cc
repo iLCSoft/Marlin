9bb305e90b876faf8a29cc9fe7b29085d7d6efbe
Ete Remi <remi.ete@desy.de>
2019-03-28 17:56:32 +0100

    Added missing MarlinMT main file

---
+++ source/main/MarlinMT.cc
@@ -0,0 +1,48 @@
+// -- marlin headers
+#include <marlin/MarlinMTApplication.h>
+#include <marlin/PluginManager.h>
+
+using namespace marlin ;
+
+#define MARLIN_MAIN_LOG( MLEVEL , message ) \
+  if ( nullptr != application ) { \
+    application->logger()->log<MLEVEL>() << message ; \
+  } \
+  else { \
+    streamlog_out(MLEVEL) << message ; \
+  }
+
+int main( int argc, char **argv ) {
+  
+  std::shared_ptr<Application> application {nullptr} ;
+  
+  // load plugins first
+  auto &mgr = PluginManager::instance() ;
+  mgr.logger()->setLevel<MESSAGE>();
+  if ( not mgr.loadLibraries() ) {
+    throw Exception( "Couldn't load shared libraries from MARLIN_DLL !" ) ;
+  }
+  // configure and run application
+  try {
+    application = std::make_shared<MarlinMTApplication>() ;    
+    application->init( argc, argv ) ;
+    application->run() ;
+  }
+  catch ( marlin::Exception &e ) {
+    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught Marlin exception " << e.what() << std::endl )
+    MARLIN_MAIN_LOG( ERROR, "Exiting with status 1" << std::endl )
+    return 1 ;
+  }
+  catch ( std::exception &e ) {
+    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught std::exception " << e.what() << std::endl )
+    MARLIN_MAIN_LOG( ERROR, "Exiting with status 1" << std::endl )
+    return 1 ;
+  }
+  catch ( ... ) {
+    MARLIN_MAIN_LOG( ERROR, "Marlin main, caught unknown exception" << std::endl )
+    MARLIN_MAIN_LOG( ERROR, "Exiting with status 2" << std::endl )
+    return 2 ;
+  }
+
+  return 0 ;
+}
