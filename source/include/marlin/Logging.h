a1a299fc4f17ad9f3925dd333ab6ca2f189fa4be
Ete Remi <remi.ete@desy.de>
2019-04-08 10:16:48 +0200

    Moved streamlog verbosity level import to Logging.h. Marked VerbosityLevels.h header as deprecated

--- source/include/marlin/Logging.h
+++ source/include/marlin/Logging.h
@@ -4,11 +4,60 @@
 // -- streamlog headers
 #include <streamlog/streamlog.h>
 
-// -- marlin headers
-#include <marlin/VerbosityLevels.h>  // import verbosity levels
-
 namespace marlin {
+  
+  // import the log level classes into the marlin namespace 
+  using  streamlog::DEBUG ;
+  using  streamlog::DEBUG0 ;
+  using  streamlog::DEBUG1 ;
+  using  streamlog::DEBUG2 ;
+  using  streamlog::DEBUG3 ;
+  using  streamlog::DEBUG4 ;
+  using  streamlog::DEBUG5 ;
+  using  streamlog::DEBUG6 ;
+  using  streamlog::DEBUG7 ;
+  using  streamlog::DEBUG8 ;
+  using  streamlog::DEBUG9 ;
+  using  streamlog::MESSAGE ;
+  using  streamlog::MESSAGE0 ;
+  using  streamlog::MESSAGE1 ;
+  using  streamlog::MESSAGE2 ;
+  using  streamlog::MESSAGE3 ;
+  using  streamlog::MESSAGE4 ;
+  using  streamlog::MESSAGE5 ;
+  using  streamlog::MESSAGE6 ;
+  using  streamlog::MESSAGE7 ;
+  using  streamlog::MESSAGE8 ;
+  using  streamlog::MESSAGE9 ;
+  using  streamlog::WARNING ;
+  using  streamlog::WARNING0 ;
+  using  streamlog::WARNING1 ;
+  using  streamlog::WARNING2 ;
+  using  streamlog::WARNING3 ;
+  using  streamlog::WARNING4 ;
+  using  streamlog::WARNING5 ;
+  using  streamlog::WARNING6 ;
+  using  streamlog::WARNING7 ;
+  using  streamlog::WARNING8 ;
+  using  streamlog::WARNING9 ;
+  using  streamlog::ERROR ;
+  using  streamlog::ERROR0 ;
+  using  streamlog::ERROR1 ;
+  using  streamlog::ERROR2 ;
+  using  streamlog::ERROR3 ;
+  using  streamlog::ERROR4 ;
+  using  streamlog::ERROR5 ;
+  using  streamlog::ERROR6 ;
+  using  streamlog::ERROR7 ;
+  using  streamlog::ERROR8 ;
+  using  streamlog::ERROR9 ;
+  using  streamlog::SILENT ;
 
+  /**
+   *  @brief  Loggin class.
+   *  Defines the basic logging functionalities in Marlin.
+   *  For a more application oriented configuration, see LoggerManager.h
+   */
   class Logging {
   public:
 #ifdef MARLIN_LOGGER_TS
