d7f82042356f13f9a68bb29386bee248e92599c5
Ete Remi <remi.ete@desy.de>
2019-04-08 09:59:12 +0200

    Moved TestProcessor class definition in cc file. Cleanup processor code

--- source/src/TestProcessor.cc
+++ source/src/TestProcessor.cc
@@ -1,19 +1,72 @@
-#include "marlin/TestProcessor.h"
-#include "marlin/Exceptions.h"
+
+// -- marlin headers
+#include <marlin/Processor.h>
+#include <marlin/Exceptions.h>
+#include <marlin/Logging.h>
+
+// -- std headers
 #include <iostream>
 #include <sstream>
 
-#include "marlin/VerbosityLevels.h"
+// -- lcio headers
+#include <lcio.h>
 
+namespace marlin {
 
-#include "streamlog/streamlog.h"
+  /** Simple processor for testing. 
+   *  Writes something to stdout for every callbackmethod.
+   * 
+   *  <h4>Input - Prerequisites</h4>
+   *  none
+   *
+   *  <h4>Output</h4> 
+   *  none
+   * 
+   * @author F. Gaede, DESY
+   * @version $Id: TestProcessor.h,v 1.7 2007-05-23 13:12:21 gaede Exp $ 
+   */
 
-namespace marlin{
+  class TestProcessor : public Processor {
+    
+  public:
+    
+    Processor*  newProcessor() { return new TestProcessor ; }
+    
+    TestProcessor() ;
+    
+    /** Called at the begin of the job before anything is read.
+     * Use to initialize the processor, e.g. book histograms.
+     */
+    void init() ;
+    
+    /** Called for every run.
+     */
+    void processRunHeader( EVENT::LCRunHeader* run ) ;
+    
+    /** Called for every event - the working horse.
+     */
+    void processEvent( EVENT::LCEvent * evt ) ; 
+    
+    
+    void check( EVENT::LCEvent * evt ) ; 
+    
+    /** Called after data processing for clean up.
+     */
+    void end() ;
+    
+  protected:
 
+    /**Test method for const.*/
+    void printEndMessage() const ;
 
-
-  TestProcessor aTestProcessor ;
-
+    int       _nRun {0} ;
+    int       _nEvt {0} ;
+    bool      _doCalibration {false} ;
+    int       _nLoops {0} ;
+  } ;
+  
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
 
   TestProcessor::TestProcessor() : Processor("TestProcessor"),
     _nRun (-1), 
@@ -24,6 +77,8 @@ namespace marlin{
     _description = "Simple processor to test the marlin application."
       " Prints run and event number." ;
   }
+  
+  //--------------------------------------------------------------------------
 
   void TestProcessor::init() { 
 
@@ -44,38 +99,24 @@ namespace marlin{
     _nEvt = 0 ;
     _doCalibration = false ;
     _nLoops = 0 ;
- 
-
   }
+  
+  //--------------------------------------------------------------------------
 
   void TestProcessor::processRunHeader( LCRunHeader* run) { 
-
-
    log<MESSAGE>() << " processRun() " 
 			    << run->getRunNumber() 		  
 			    << std::endl ;
     
     _nRun++ ;
-  } 
+  }
+  
+  //--------------------------------------------------------------------------
 
   void TestProcessor::processEvent( LCEvent * evt ) { 
-
-
     _nEvt ++ ;
-
     //------------ example code that tests/demonstrates ------------
     //------------ the usage of Exceptions to steer program flow ------------
-    //   
-    //  --- skip every 7th event --------------
-
-    //if( !(  _nEvt % 7 )  )
-    //      throw SkipEventException( this ) ;
-  
-    //  ---  stop afteer processing 80 events -----
-
-    //     if( _nEvt == 80   )
-    //      throw StopProcessingException( this ) ;
-
     // --- loop 3 times over the first 3 events and do a 'calibration' ) ------
 
     if( isFirstEvent() ){
@@ -92,82 +133,61 @@ namespace marlin{
 
       setReturnValue( "Calibrating" , true ) ;
 
-      //      message<MESSAGE>(  log()
       log<MESSAGE>() << "processEvent()  ---CALIBRATING ------ "  
 			      << " in event " << evt->getEventNumber() << " (run " 
 			      << evt->getRunNumber() << ") " 
 			      << std::endl ;
-      
-// 	) ;
 
-
-      // your calibration goes here ....
-    
-
-    
-      // --------------- 
-      if( _nEvt == 3 ){  
-      
-	_nEvt = 0 ;
-	++_nLoops ;
-      
-	if( _nLoops == 3 ){
-	
-	  _doCalibration = false ;
-	
-	}else{
-	
-	  throw RewindDataFilesException( this ) ;
-	}
+      if( _nEvt == 3 ) {
+      	_nEvt = 0 ;
+      	++_nLoops ;
+      	if( _nLoops == 3 ) {
+      	  _doCalibration = false ;
+      	}
+        else {
+      	  throw RewindDataFilesException( this ) ;
+      	}
       }
     }
-  
-    //---------end example code  ----------------------------------------
 
     log<MESSAGE>() << " processing event " << evt->getEventNumber() 
 			   << "  in run "          << evt->getRunNumber() 
 			   << std::endl ;
     
-    
-//     log<DEBUG>()   << "(DEBUG)   local verbosity level: " << logLevelName() << std::endl ;
     log<MESSAGE>() << "(MESSAGE) local verbosity level: " << logLevelName() << std::endl ;
-//     streamlog_out(WARNING) << "(WARNING) local verbosity level: " << logLevelName() << std::endl ;
-//     streamlog_out(ERROR)   << "(ERROR)   local verbosity level: " << logLevelName() << std::endl ;
-
-
-
 
     // always return true  for ProcessorName
     setReturnValue( true ) ;
     
     // set ProcessorName.EvenNumberOfEvents == true if this processor has been called 2n (n=0,1,2,...) times 
     setReturnValue("EvenNumberOfEvents",  !( _nEvt % 2 )   ) ;
-
-    
   }
   
+  //--------------------------------------------------------------------------
+  
   void TestProcessor::check( LCEvent * evt ) { 
-    
-    log<DEBUG>() << " check() "  // << dummy_method() 
+    log<DEBUG>() << " check() "
 			 << evt->getEventNumber() 
 			 << " (run " << evt->getRunNumber() << ") "
 			 << std::endl ;
-    
-    
   }
   
+  //--------------------------------------------------------------------------
+  
   void TestProcessor::end(){ 
-    
     printEndMessage() ;
-
   }
+  
+  //--------------------------------------------------------------------------
 
   void TestProcessor::printEndMessage() const {
-
     log<MESSAGE>() << " end() "  
 			   << " processed "     << _nEvt << " events in " 
 			   << _nRun << " runs " << std::endl 
 			   << std::endl ;
   }
+  
+  // processor declaration
+  TestProcessor aTestProcessor ;
   
 }// namespace marlin
