dde2428831f3c5263237ed2d8be1ad89346b9ee2
Ete Remi <remi.ete@desy.de>
2019-04-23 17:17:52 +0200

    Added generic ReaderListener implementation

---
+++ source/include/marlin/ReaderListener.h
@@ -0,0 +1,79 @@
+#ifndef MARLIN_READERLISTENER_h
+#define MARLIN_READERLISTENER_h 1
+
+// -- lcio headers
+#include <MT/LCReader.h>
+
+// -- std headers
+#include <functional>
+
+namespace marlin {
+  
+  /**
+   *  @brief  ReaderListener class.
+   *  
+   *  Simple implementation of a reader listener.
+   *  Callback functions can be set using lambda function, std::function objects
+   *  or resulting call of std::bind call. 
+   *  
+   *  Example with lambda functions:
+   *  @code{cpp}
+   *  ReaderListener listener ;
+   *  listener.onEventRead( [](std::shared_ptr<EVENT::LCEvent> event){
+   *    std::cout << "Read event no " << event->getEventNumber() << std::endl ;
+   *  }) ;
+   *  listener.onRunHeaderRead( [](std::shared_ptr<EVENT::LCRunHeader> rhdr){
+   *    std::cout << "Read run header no " << rhdr->getRunNumber() << std::endl ;
+   *  }) ;
+   *  @endcode
+   *
+   *  Example with std::bind and custom class method:
+   *  @code{cpp}
+   *  UserClass user ;
+   *  ReaderListener listener ;
+   *  listener.onEventRead( std::bind(&UserClass::processEvent, &user) ) ;
+   *  listener.onRunHeaderRead( std::bind(&UserClass::processRunHeader, &user) ) ;
+   *  @endcode
+   *
+   * Note that the current implementation forward the processing only on 
+   * modifyEvent() and modifyRunHeader(). Thus the data can be modified in
+   * callback functions. This is a requirement for concurrent applications
+   */
+  class ReaderListener : public MT::LCReaderListener {
+  public:
+    using LCEventFunction = std::function<void(std::shared_ptr<EVENT::LCEvent>)> ;
+    using LCRunHeaderFunction = std::function<void(std::shared_ptr<EVENT::LCRunHeader>)> ;
+    
+  public:
+    ReaderListener() = default ;
+    ~ReaderListener() = default ;
+    ReaderListener(const ReaderListener&) = delete ;
+    ReaderListener &operator=(const ReaderListener&) = delete ;
+    
+    /**
+     *  @brief  Set the callback function to process on event read
+     */
+    void onEventRead( LCEventFunction func ) ;
+    
+    /**
+     *  @brief  Set the callback function to process on run header read
+     */
+    void onRunHeaderRead( LCRunHeaderFunction func ) ;
+    
+  private:
+    // from MT::LCReaderListener
+    void processEvent( std::shared_ptr<EVENT::LCEvent> event ) override ;
+    void modifyEvent( std::shared_ptr<EVENT::LCEvent> event ) override ;
+    void processRunHeader( std::shared_ptr<EVENT::LCRunHeader> hdr ) override ;
+    void modifyRunHeader( std::shared_ptr<EVENT::LCRunHeader> hdr ) override ;
+    
+  private:
+    ///< Callback function on event read
+    LCEventFunction        _onEventRead {nullptr} ;
+    ///< Callback function on run header read
+    LCRunHeaderFunction    _onRunHeaderRead {nullptr} ;
+  };
+  
+}
+
+#endif
---
+++ source/src/ReaderListener.cc
@@ -0,0 +1,43 @@
+#include <marlin/ReaderListener.h>
+
+namespace marlin {
+
+  void ReaderListener::onEventRead( LCEventFunction func ) {
+    _onEventRead = func ;
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void ReaderListener::onRunHeaderRead( LCRunHeaderFunction func ) {
+    _onRunHeaderRead = func ;
+  }
+  
+  //--------------------------------------------------------------------------
+
+  void ReaderListener::processEvent( std::shared_ptr<EVENT::LCEvent> /*event*/ ) {
+    /* nop */
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void ReaderListener::modifyEvent( std::shared_ptr<EVENT::LCEvent> event ) {
+    if( nullptr != _onEventRead ) {
+      _onEventRead( event ) ;
+    }
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void ReaderListener::processRunHeader( std::shared_ptr<EVENT::LCRunHeader> /*hdr*/ ) {
+    /* nop */
+  }
+  
+  //--------------------------------------------------------------------------
+  
+  void ReaderListener::modifyRunHeader( std::shared_ptr<EVENT::LCRunHeader> rhdr ) {
+    if( nullptr != _onRunHeaderRead ) {
+      _onRunHeaderRead( rhdr ) ;
+    }
+  }
+
+}
