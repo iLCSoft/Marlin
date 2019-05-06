fd14f0f8a5404a6030a66a059ee7772bac3fd8e5
Remi Ete <remi.ete@gmail.com>
2019-05-06 02:01:13 +0200

    Refactored StringParameters to something nicer. Breaks backward compatibility though ...

--- source/include/marlin/StringParameters.h
+++ source/include/marlin/StringParameters.h
@@ -1,72 +1,289 @@
-#ifndef StringParameters_h
-#define StringParameters_h 1
+#ifndef MARLIN_STRINGPARAMETERS_h
+#define MARLIN_STRINGPARAMETERS_h 1
 
-#include "lcio.h"
-
-#ifdef LCIO_MAJOR_VERSION 
- #if LCIO_VERSION_GE( 1,2)  
-  #include "LCIOSTLTypes.h"
- #endif
-#else
-  #include "MarlinLCIOSTLTypes.h"
-#endif
-
-
+// -- std headers
 #include <vector>
 #include <string>
 #include <map>
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 
+// -- marlin headers
+#include <marlin/Utils.h>
 
-typedef std::map< std::string ,  std::vector< std::string > >  ParametersMap ;
+namespace marlin {
 
+  /** Simple parameters class for Marlin.
+   *  Holds named parameters as string vectors.
+   *  @author F. Gaede, DESY
+   *  @author R. Ete, DESY
+   *  @version $Id: StringParameters.h,v 1.5 2006-11-10 11:56:07 engels Exp $
+   */
+  class StringParameters {
+    typedef std::map<std::string, std::vector<std::string>> ParametersMap ;
+    friend std::ostream& operator<< ( std::ostream& , const StringParameters& ) ;
 
-using namespace lcio ;
+  public:
+    StringParameters() = default ;
+    ~StringParameters() = default ;
+    StringParameters( const StringParameters &sp ) = default ;
 
-namespace marlin{
+    /**
+     *  @brief  Add a parameter without value.
+     *  Does nothing if the parameter exists
+     *
+     *  @param  key the prameter key
+     */
+    void add( const std::string& key ) ;
 
-/** Simple parameters class for Marlin.
- *  Holds named parameters as string vectors.
- *  @author F. Gaede, DESY
- *  @version $Id: StringParameters.h,v 1.5 2006-11-10 11:56:07 engels Exp $ 
- */
+    /**
+     *  @brief  Add a parameter with its value. Note that the value is
+     *  appended to the parameter list. To overwrite a parameter value,
+     *  use replace() instead
+     *
+     *  @param  key the parameter key
+     *  @param  value the parameter value
+     */
+    template <typename T>
+    void add( const std::string& key, const T &value ) ;
 
-class StringParameters {
-  
-  friend std::ostream& operator<< (  std::ostream& , StringParameters& ) ;
+    /**
+     *  @brief  Add a parameter with its values. Note that the values are
+     *  appended to the parameter list. To overwrite a parameter value,
+     *  use replace() instead
+     *
+     *  @param  key the parameter key
+     *  @param  values the parameter values
+     */
+    template <typename T>
+    void add( const std::string& key, const std::vector<T> &values ) ;
 
-public:
-  
-  StringParameters() ; 
-  StringParameters( const StringParameters &sp ) ; 
+    /**
+     *  @brief  Replace a parameter with a new value.
+     *
+     *  @param  key the parameter key
+     *  @param  value the parameter value
+     */
+    template <typename T>
+    void replace( const std::string& key, const T &value ) ;
 
-  void add( const std::string& key , const std::vector<std::string> & values ) ;
-  void add( const std::vector<std::string> & tokens ) ;
+    /**
+     *  @brief  Replace a parameter with a new values.
+     *
+     *  @param  key the parameter key
+     *  @param  values the parameter values
+     */
+    template <typename T>
+    void replace( const std::string& key, const std::vector<T> &values ) ;
 
-  void erase( const std::string& key ) ;
+    /**
+     *  @brief  Erase a parameter
+     *
+     *  @param  key the parameter key
+     */
+    void erase( const std::string& key ) ;
 
-  bool isParameterSet(  const std::string& key ) ;
+    /**
+     *  @brief  Whether the parameter is set, meaning the entry already exists,
+     *  but has no value. To check if a parameter exists, use exists() instead
+     *
+     *  @param  key the parameter key
+     */
+    bool isParameterSet( const std::string& key ) const ;
 
-  int   getIntVal( const std::string& key ) ;
-  float getFloatVal( const std::string& key ) ;
-  const std::string& getStringVal( const std::string& key ) ;
+    /**
+     *  @brief  Whether the parameter exists.
+     *
+     *  @param  key the parameter key
+     */
+    bool exists( const std::string& key ) const ;
 
-  IntVec& getIntVals( const std::string& key ,  IntVec& intVec ) ;
-  FloatVec& getFloatVals( const std::string& key ,  FloatVec& floatVec ) ;
-  StringVec & getStringVals( const std::string& key  , StringVec & stringVec ) ;
+    /**
+     *  @brief  Unset a parameter
+     *
+     *  @param  key the parameter key
+     */
+    void unset( const std::string &key ) ;
 
-  StringVec& getStringKeys( StringVec& stringVec );
-  
-  virtual ~StringParameters() ; 
-  
-protected:
-  ParametersMap _map{};
+    /**
+     *  @brief  Unset all parameters
+     */
+    void unset() ;
 
-};
+    /**
+     *  @brief  Get the parameter keys
+     */
+    std::vector<std::string> keys() const ;
 
-int intVal( const std::string& str) ;
-float floatVal( const std::string& str) ;
-std::ostream& operator<< (  std::ostream& s, StringParameters& p ) ;
+    /**
+     *  @brief  Get a parameter value. If the value doesn't exists, an
+     *  exception is thrown.
+     *
+     *  @param  key the parameter key
+     */
+    template <typename T>
+    T getValue( const std::string& key ) const ;
 
-} // end namespace marlin 
+    /**
+     *  @brief  Get parameter values. If the values do not exists, an
+     *  exception is thrown.
+     *
+     *  @param  key the parameter key
+     */
+    template <typename T>
+    std::vector<T> getValues( const std::string& key ) const ;
+
+    /**
+     *  @brief  Get a parameter value. If the value doesn't exists, the fallback
+     *  value provided as second argument is returned
+     *
+     *  @param  key the parameter key
+     *  @param  fallback the fallback value if not found
+     */
+    template <typename T>
+    T getValue( const std::string& key , const T &fallback ) const ;
+
+    /**
+     *  @brief  Get parameter values. If the values do not exists, the fallback
+     *  values provided as second argument is returned
+     *
+     *  @param  key the parameter key
+     *  @param  fallback the fallback value if not found
+     */
+    template <typename T>
+    std::vector<T> getValues( const std::string& key , const std::vector<T> &fallback ) const ;
+
+    /**
+     *  @brief  Get a parameter value by reference
+     *
+     *  @param  key the parameter key
+     *  @param  value the value to receive
+     */
+    template <typename T>
+    void get( const std::string& key , T &value ) const ;
+
+    /**
+     *  @brief  Get parameter values by reference
+     *
+     *  @param  key the parameter key
+     *  @param  values the values to receive
+     */
+    template <typename T>
+    void get( const std::string& key , std::vector<T> &values ) const ;
+
+  protected:
+    ///< The parameters map
+    ParametersMap          _map {} ;
+  };
+
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+
+  std::ostream& operator<< (  std::ostream& s, const StringParameters& p ) ;
+
+  //--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::add( const std::string& key, const T &value ) {
+    add( key ) ;
+    auto iter = _map.find( key ) ;
+    iter->second.push_back( StringUtil::typeToString<T>( value ) )  ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::add( const std::string& key, const std::vector<T> &values ) {
+    add( key ) ;
+    auto iter = _map.find( key ) ;
+    for( auto val : values ) {
+      iter->second.push_back( StringUtil::typeToString<T>(val) ) ;
+    }
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::replace( const std::string& key, const T &value ) {
+    erase( key ) ;
+    add( key, value ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::replace( const std::string& key, const std::vector<T> &values ) {
+    erase( key ) ;
+    add( key , values ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline T StringParameters::getValue( const std::string& key ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      throw Exception( "StringParameters::getAs: parameter '" + key + "' not found" ) ;
+    }
+    return StringUtil::stringToType<T>( iter->second.at(0) ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline std::vector<T> StringParameters::getValues( const std::string& key ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      throw Exception( "StringParameters::getAs: parameter '" + key + "' not found" ) ;
+    }
+    return StringUtil::stringToType<T>( iter->second ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline T StringParameters::getValue( const std::string& key , const T &fallback ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      return fallback ;
+    }
+    return StringUtil::stringToType<T>( iter->second.at(0) ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline std::vector<T> StringParameters::getValues( const std::string& key , const std::vector<T> &fallback ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      return fallback ;
+    }
+    return StringUtil::stringToType<T>( iter->second ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::get( const std::string& key , T &value ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      return ;
+    }
+    value = StringUtil::stringToType<T>( iter->second.at(0) ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  template <typename T>
+  inline void StringParameters::get( const std::string& key , std::vector<T> &values ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      return ;
+    }
+    values = StringUtil::stringToType<T>( iter->second ) ;
+  }
+
+} // end namespace marlin
 #endif
--- source/src/StringParameters.cc
+++ source/src/StringParameters.cc
@@ -1,174 +1,76 @@
-#include "marlin/StringParameters.h"
+#include <marlin/StringParameters.h>
 
+// -- std headers
 #include <sstream>
-#include <algorithm>
 #include <exception>
-#include <iomanip>
 
-namespace marlin{
+namespace marlin {
 
-StringParameters::StringParameters(){
-
-}
-
-  StringParameters::StringParameters( const StringParameters& sp ):
-    _map(sp._map){
+  void StringParameters::add( const std::string& key ) {
+    _map.insert( ParametersMap::value_type(key, ParametersMap::mapped_type()) ) ;
   }
 
-StringParameters::~StringParameters(){
+  //--------------------------------------------------------------------------
 
-}
-
-
-void StringParameters::add( const std::string& key , const std::vector<std::string> & values ) {
-
-  for(unsigned int i=0 ; i< values.size() ; i++){
-    
-    _map[ key ].push_back( values[i] ) ;
-  }
-}
-
-void StringParameters::add( const std::vector<std::string> & tokens ) {
-  
-  for(unsigned int i= 1 ; i< tokens.size() ; i++){
-    
-    _map[ tokens[0]  ].push_back( tokens[i] ) ;
-  }
-}
-
-void StringParameters::erase( const std::string& key ) {
-    
-  _map.erase( key );
-}
-
-bool StringParameters::isParameterSet(  const std::string& key ) {
-
-  return _map[ key ].size() > 0 ;
-}
-
-int StringParameters::getIntVal( const std::string& key ) {
-  
-  if( _map[ key ].size() == 0 ) return 0 ;
-
-  return intVal( _map[ key ][0] ) ;
-}
-
-float StringParameters::getFloatVal( const std::string& key ) {
-
-  if( _map[ key ].size() == 0 ) return 0 ;
-  
-  return floatVal(  _map[ key ][0] ) ;
-}
-
-
-const std::string& StringParameters::getStringVal( const std::string& key )  {
-
-  static std::string empty("")  ;
-  if( _map[ key ].size() == 0 ) return empty ;
-  return  _map[ key ][0] ;
-}
-
-
-
-IntVec& StringParameters::getIntVals( const std::string& key ,  IntVec& intVec ) {
-
-  transform(  _map[ key ].begin() , _map[ key ].end()  , back_inserter(intVec) , intVal ) ;
-  
-  return intVec ;
-}
-
-
-FloatVec&  StringParameters::getFloatVals( const std::string& key  ,  FloatVec& floatVec ){
-
-  transform(  _map[ key ].begin() , _map[ key ].end()  , back_inserter(floatVec) , floatVal ) ;
-
-  return floatVec ;
-}
-
-
-StringVec& StringParameters::getStringVals( const std::string& key  , StringVec& stringVec ) {
-
-  copy( _map[ key ].begin() , _map[ key ].end()  , back_inserter(stringVec) ) ;
-
-  return  stringVec ;
-}
-
-StringVec& StringParameters::getStringKeys( StringVec& stringVec ) {
-
-  for(ParametersMap::iterator m = _map.begin() ; m != _map.end() ; m++){
-    stringVec.push_back( m->first ); 
-  }
-  
-  return  stringVec ;
-}
-
-// FIXME where should this utility function live ?
-template <class T>
-bool convert(std::string input, T &value) {
-
- std::istringstream stream(input);
-
- return ( ! (stream >> std::setbase(0) >> value).fail() ) && stream.eof();
-
-}
-
-
-int intVal(  const std::string& str ){
-
-  static int i;
-
-  if( convert( str , i ) ){
-      //std::cout << "converted : " << f << std::endl ;
-      return i;
-  }
-  else{
-      
-      std::stringstream sstr;
-
-      sstr << "failed converting int from string: " << str << std::endl ;
-
-      throw Exception( sstr.str() );
+  void StringParameters::erase( const std::string& key ) {
+    _map.erase( key );
   }
 
+  //--------------------------------------------------------------------------
 
-}
-
-float floatVal( const std::string& str ){
-
-  // atof would not give an error trying to convert from string: "1.003.4"
-  //return atof( str.c_str() )  ; 
-
-  static float f;
-
-  if( convert( str , f ) ){
-      //std::cout << "converted : " << f << std::endl ;
-      return f;
-  }
-  else{
-      
-      std::stringstream sstr;
-
-      sstr << "failed converting float from string: " << str << std::endl ;
-
-      throw Exception( sstr.str() );
-  }
-}
-
-std::ostream& operator<< (  std::ostream& s,  StringParameters& p ) {
-
-  for(ParametersMap::iterator m = p._map.begin() ; m != p._map.end() ; m++){
-
-    s << "      " << m->first << ": " ;  
-
-    for(unsigned int i=0; i < m->second.size(); i++ ){
-
-      s << "[" << m->second[i] << "]  " ;
-      
+  bool StringParameters::isParameterSet(  const std::string& key ) const {
+    auto iter = _map.find( key ) ;
+    if( _map.end() == iter ) {
+      return false ;
     }
-    s << std::endl ;
-    
+    return (not iter->second.empty() ) ;
   }
-  return s ;
-}
+
+  //--------------------------------------------------------------------------
+
+  bool StringParameters::exists( const std::string& key ) const {
+    return (_map.find( key ) != _map.end() ) ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  void StringParameters::unset( const std::string &key ) {
+    auto iter = _map.find( key ) ;
+    if( _map.end() != iter ) {
+      iter->second.clear() ;
+    }
+  }
+
+  //--------------------------------------------------------------------------
+
+  void StringParameters::unset() {
+    for( auto iter : _map ) {
+      iter.second.clear() ;
+    }
+  }
+
+  //--------------------------------------------------------------------------
+
+  std::vector<std::string> StringParameters::keys() const {
+    std::vector<std::string> result ;
+    result.reserve( _map.size() ) ;
+    for( auto m : _map ) {
+      result.push_back( m.first );
+    }
+    return result ;
+  }
+
+  //--------------------------------------------------------------------------
+
+  std::ostream& operator<< (  std::ostream& s,  const StringParameters& p ) {
+    for( auto m : p._map ) {
+      s << "      " << m.first << ": " ;
+      for( auto val : m.second ) {
+        s << "[" << val << "]  " ;
+      }
+      s << std::endl ;
+    }
+    return s ;
+  }
 
 } //namsepace marlin
