7e839001d1535287003a5ed7fd1cff9c13bae55d
Remi Ete <remi.ete@gmail.com>
2019-05-09 19:57:02 +0200

    Use new API for random seeds

--- test/marlintest/src/TestProcessorEventSeeder.cc
+++ test/marlintest/src/TestProcessorEventSeeder.cc
@@ -2,7 +2,7 @@
 
 // ----- include for verbosity dependend logging ---------
 #include "marlin/Logging.h"
-#include "marlin/EventExtensions.h"
+#include "marlin/ProcessorApi.h"
 
 #include "IMPL/LCEventImpl.h"
 #include "IMPL/LCRunHeaderImpl.h"
@@ -15,112 +15,108 @@ TestProcessorEventSeeder aTestProcessorEventSeeder ;
 
 
 TestProcessorEventSeeder::TestProcessorEventSeeder() : Processor("TestProcessorEventSeeder") {
-  
+
   // modify processor description
   _description = "TestProcessorEventSeeder test the uniquie event seeding functionality of the ProcessorEventSeeder service in Marlin" ;
 
   _nRun = -1 ;
   _nEvt = -1 ;
-  
+
 }
 
 
-void TestProcessorEventSeeder::init() { 
+void TestProcessorEventSeeder::init() {
 
-  // TODO use new registration API
-  // Global::EVENTSEEDER->registerProcessor(this);
-  
+  ProcessorApi::registerForRandomSeeds( this ) ;
+
   _nRun = 0 ;
   _nEvt = 0 ;
 }
 
 
-void TestProcessorEventSeeder::processRunHeader( EVENT::LCRunHeader* ) { 
-  
+void TestProcessorEventSeeder::processRunHeader( EVENT::LCRunHeader* ) {
+
   ++_nRun ;
 
-} 
+}
 
-void TestProcessorEventSeeder::processEvent( EVENT::LCEvent * evt ) { 
+void TestProcessorEventSeeder::processEvent( EVENT::LCEvent * evt ) {
 
 
-  streamlog_out(DEBUG) << "   processing event: " << evt->getEventNumber() 
-		       << "   in run:  " << evt->getRunNumber() 
+  streamlog_out(DEBUG) << "   processing event: " << evt->getEventNumber()
+		       << "   in run:  " << evt->getRunNumber()
 		       << std::endl ;
-  
-  auto randomSeeds = evt->runtime().ext<RandomSeed>() ;
-  unsigned int seed = randomSeeds->randomSeed( this ) ;
 
-  streamlog_out( DEBUG ) << "seed set to " 
+  unsigned int seed = ProcessorApi::getRandomSeed( this, evt ) ;
+
+  streamlog_out( DEBUG ) << "seed set to "
 			    << seed
 			    << " for event "
 			    << evt->getEventNumber()
 			    << std::endl;
 
   try{
-    // TODO try again to register processor with new API
-    // Global::EVENTSEEDER->registerProcessor(this);
+    ProcessorApi::registerForRandomSeeds( this ) ;
   }
   catch( lcio::Exception ) {
     log<ERROR>() << name() << " failed to register processor to event seed generator (TEST is OK)" << std::endl ;
   }
 
-  unsigned long long runnum_and_eventnum = evt->getRunNumber() ; 
+  unsigned long long runnum_and_eventnum = evt->getRunNumber() ;
   runnum_and_eventnum = runnum_and_eventnum << 32  ;
-  
+
   runnum_and_eventnum += evt->getEventNumber() ;
 
   std::map< unsigned long long, unsigned int>::iterator it ;
-  
+
   it = _seeds.find(runnum_and_eventnum);
 
   if( it == _seeds.end() ){
-    _seeds[runnum_and_eventnum] = seed ; 
-  } 
+    _seeds[runnum_and_eventnum] = seed ;
+  }
   else {
 
     if( seed != _seeds[runnum_and_eventnum] ) {
       streamlog_out(ERROR) << " Seeds don't match for"
 			   << " run " <<   evt->getRunNumber()
 			   << " event " << evt->getEventNumber()
-			   << " old seed = " << _seeds[runnum_and_eventnum] 
-			   << " new seed = " << seed  
-			   << std::endl ;      
+			   << " old seed = " << _seeds[runnum_and_eventnum]
+			   << " new seed = " << seed
+			   << std::endl ;
     }
     else{
       streamlog_out(DEBUG) << " Seeds match for"
 			   << " run " <<   evt->getRunNumber()
 			   << " event " << evt->getEventNumber()
-			   << " old seed = " << _seeds[runnum_and_eventnum] 
-			   << " new seed = " << seed  
-			   << std::endl ;      
+			   << " old seed = " << _seeds[runnum_and_eventnum]
+			   << " new seed = " << seed
+			   << std::endl ;
 
     }
-    
+
   }
 
 //  if( _nEvt > 10 ) {
-      
+
   //  streamlog_out(ERROR)  << " processEvent LCEvent::eventNumber not modified ! "  << std::endl;
 
   //-- note: this will not be printed if compiled w/o MARLINDEBUG=1 !
-  
+
   ++_nEvt ;
-  
+
 }
 
 
 
-void TestProcessorEventSeeder::check( LCEvent * ) { 
+void TestProcessorEventSeeder::check( LCEvent * ) {
   // nothing to check here - could be used to fill checkplots in reconstruction processor
 }
 
 
-void TestProcessorEventSeeder::end(){ 
-  
-  streamlog_out(MESSAGE4) << name() 
+void TestProcessorEventSeeder::end(){
+
+  streamlog_out(MESSAGE4) << name()
 			  << " random seeds received for " << _nEvt << " events in " << _nRun << " run "
 			  << std::endl ;
-  
-}
 
+}
