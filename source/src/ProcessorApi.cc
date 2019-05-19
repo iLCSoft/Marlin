47835b6cf353ad3f0c63c5e209f71f47f1f1cabd
Remi Ete <remi.ete@gmail.com>
2019-05-19 17:42:49 +0200

    Use MARLIN_GEAR variable to compile Gear geometry getter

--- source/src/ProcessorApi.cc
+++ source/src/ProcessorApi.cc
@@ -15,16 +15,18 @@
 #endif
 
 // -- gear headers
+#ifdef MARLIN_GEAR
 #include <gear/GearMgr.h>
+#endif
 
 namespace marlin {
 
   void ProcessorApi::registerForRandomSeeds( Processor *const proc ) {
     proc->app().randomSeedManager().addEntry( proc ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   unsigned int ProcessorApi::getRandomSeed( const Processor *const proc, EVENT::LCEvent *event ) {
     auto randomSeeds = event->runtime().ext<RandomSeed>() ;
     if( nullptr == randomSeeds ) {
@@ -32,9 +34,9 @@ namespace marlin {
     }
     return randomSeeds->randomSeed( proc ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void ProcessorApi::setReturnValue( const Processor *const proc, EVENT::LCEvent *event, bool value ) {
     auto procConds = event->runtime().ext<ProcessorConditions>() ;
     if( nullptr == procConds ) {
@@ -42,9 +44,9 @@ namespace marlin {
     }
     return procConds->set( proc, value ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void ProcessorApi::setReturnValue( const Processor *const proc, EVENT::LCEvent *event, const std::string &name, bool value ) {
     auto procConds = event->runtime().ext<ProcessorConditions>() ;
     if( nullptr == procConds ) {
@@ -52,13 +54,13 @@ namespace marlin {
     }
     return procConds->set( proc, name, value ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   bool ProcessorApi::isFirstEvent( EVENT::LCEvent *event ) {
     return event->runtime().ext<IsFirstEvent>() ;
   }
-  
+
   //--------------------------------------------------------------------------
 #ifdef MARLIN_DD4HEP
   const dd4hep::Detector* ProcessorApi::dd4hepDetector( const Processor *const proc ) {
@@ -66,20 +68,20 @@ namespace marlin {
   }
 #endif
   //--------------------------------------------------------------------------
-  
+#ifdef MARLIN_GEAR
   const gear::GearMgr* ProcessorApi::gearDetector( const Processor *const proc ) {
     return proc->app().geometryManager().geometry<gear::GearMgr>() ;
   }
-  
+#endif
   //--------------------------------------------------------------------------
-  
+
   void ProcessorApi::skipCurrentEvent( const Processor *const proc ) {
     proc->log<WARNING>() << "Skipping current event !" << std::endl ;
     throw SkipEventException( proc ) ;
   }
-  
+
   //--------------------------------------------------------------------------
-  
+
   void ProcessorApi::abort( const Processor *const proc, const std::string &reason ) {
     proc->log<WARNING>() << "Stopping application: " << reason << std::endl ;
     throw StopProcessingException( proc ) ;
