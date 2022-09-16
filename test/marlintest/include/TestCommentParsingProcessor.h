0b61acf461c05c6a66dc87b088dbde7445944a54
Thomas Madlener <thomas.madlener@desy.de>
2022-09-16 09:58:28 +0200

    Add a test processor for the comment parsing

---
+++ test/marlintest/include/TestCommentParsingProcessor.h
@@ -0,0 +1,32 @@
+#ifndef TestCommentParsingProcessor_h
+#define TestCommentParsingProcessor_h
+
+#include "marlin/Processor.h"
+
+#include "lcio.h"
+
+#include <string>
+#include <vector>
+
+/** Test processor for Marlin that can be used to check whether comments in the
+ * xml steering files are handled correctly (and as expected).
+ *
+ */
+class TestCommentParsingProcessor : public marlin::Processor {
+public:
+  Processor *newProcessor() override { return new TestCommentParsingProcessor; }
+
+  TestCommentParsingProcessor();
+
+  void init() override;
+
+private:
+  /// Vectors for different parameters that are read from the steering file
+  std::vector<std::string> m_leadingComment{};
+  std::vector<std::string> m_trailingComment{};
+  std::vector<std::string> m_mixedComment{};
+  std::vector<std::string> m_noComment{};
+  std::vector<std::string> m_onlyComment{};
+};
+
+#endif
